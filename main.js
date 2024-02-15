893b0895f0e1ada0a1a77434e32e6906691a5063
Mustadir Mahmood <mustadir.mahmood@selise.ch>
2024-02-15 13:44:03 +0600

    fix: twist baselength decimal update

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ async (dataString) => {
       limitName: "Twist",
       limitType: "",
       baseLengthUnit: HeaderTableUnitAttributes["BaseLength"],
-      columnName: `${ChartTableAttributes.Twist} ${TwistBaseLength.ReportValue} ${HeaderTableUnitAttributes["BaseLength"]}`,
+      columnName: `${ChartTableAttributes.Twist} ${TwistBaseLength.FormattedReportValue} ${HeaderTableUnitAttributes["BaseLength"]}`,
       unit: HeaderTableUnitAttributes["Twist"],
     },
     {
